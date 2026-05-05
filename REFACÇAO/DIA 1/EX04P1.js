ef08d3ff2ad44a82009e9772fbc9c9b9b56d6ffa
Julia Oliveira <juliaoliveiradossantos015@gmail.com>
2026-05-04 22:20:50 -0300

    Refactor 'revelar' function for improved readability
    Refactored the 'revelar' function to use constants for DOM elements and remove placeholder classes.

--- REFACÇAO/DIA 1/EX04P1.js
+++ REFACÇAO/DIA 1/EX04P1.js
@@ -1,9 +1,21 @@
-function revelar() {
+function revelar(){
     document.querySelector(".card-img-top").src = "img/_vinicius_junior.png";
 
-    document.getElementById("Nome").innerHTML = "Vinícius José Paixão de Oliveira Júnior";
-    document.getElementById("Rank").innerHTML = "9,5";
-    document.getElementById("Data_Nas").innerHTML = "12/07/2000 (25 anos)";
-    document.getElementById("Alutra").innerHTML = "1,76 m";
-    document.getElementById("Posicao").innerHTML = "Ponta-esquerda / Atacante";
+    const nome = document.getElementById("Nome");
+    const rank = document.getElementById("Rank");
+    const data = document.getElementById("Data_Nas");
+    const alutra = document.getElementById("Alutra");
+    const Posicao = document.getElementById("Posicao");
+
+    nome.innerHTML = "Vinícius José Paixão de Oliveira Júnior";
+    rank.innerHTML = "9,5";
+    data.innerHTML = '12/07/2000 (25 anos)';
+    alutra.innerHTML = "1,76 m";
+    Posicao.innerHTML = "Ponta-esquerda / Atacante";
+
+    nome.classList.remove('placeholder', 'col-6');
+    rank.classList.remove('placeholder', 'col-6');
+    data.classList.remove('placeholder', 'col-6');
+    alutra.classList.remove('placeholder', 'col-6');
+    Posicao.classList.remove('placeholder', 'col-6');
 }
